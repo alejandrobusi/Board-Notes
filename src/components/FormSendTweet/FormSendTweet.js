ae4ec3fbb432563809e0566bb894a8c5bcd6ddba
AlejandroBusi <alejandro_busi@hotmail.com>
2022-04-11 12:49:07 -0300

    create components

--- src/components/FormSendTweet/FormSendTweet.js
+++ src/components/FormSendTweet/FormSendTweet.js
@@ -1,21 +1,40 @@
-import react from 'react';
-import { FormControl, FormGroup, TextField, Button } from '@mui/material';
+import react from "react";
+import { FormControl, FormGroup, TextField, Button } from "@mui/material";
 
-import './FormSendTweet.scss';
+import "./FormSendTweet.scss";
 
-export default function FormSendTweet (props) {
-  const {} = props;
-  
+export default function FormSendTweet(props) {
+  const { sendTweet } = props;
+
   return (
-    <div className='form-send-tweet'>
-      <h2 className='form-send-tweet__title'>Enviar Tweet</h2>
-      <form className='form-send-tweet__form'>
+    <div className="form-send-tweet">
+      <h2 className="form-send-tweet__title">Enviar Tweet</h2>
+      <form
+        className="form-send-tweet__form"
+        onSubmit={(event) => sendTweet(event)}
+      >
         <FormControl>
           <FormGroup>
-            <TextField className='form-send-tweet__form-name' type='text' name='name' placeholder='Nombre de usuario' margin='normal'/>
+            <TextField
+              className="form-send-tweet__form-name"
+              type="text"
+              name="name"
+              placeholder="Nombre de usuario"
+              margin="normal"
+            />
           </FormGroup>
           <FormGroup>
-            <TextField className='form-sendTweet__form-textarea' name='tweet' multiline rows='6' placeholder='Escribe el tweet!' margin='normal'/>
+            <TextField
+              className="form-sendTweet__form-textarea"
+              name="tweet"
+              multiline
+              rows="6"
+              placeholder="Escribe el tweet!"
+              margin="normal"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Button type="submit">Enviar Tweet</Button>
           </FormGroup>
         </FormControl>
       </form>
--- src/components/SendTweet/SendTweet.js
+++ src/components/SendTweet/SendTweet.js
@@ -1,30 +1,39 @@
-import React, { useState } from 'react';
-import './SendTweet.scss';
-import { Fab } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import moment from 'moment'
-import ModalContainer from '../ModalContainer';
-import FormSendTweet from '../FormSendTweet'
+import React, { useState } from "react";
+import "./SendTweet.scss";
+import { Fab } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import moment from "moment";
+import ModalContainer from "../ModalContainer";
+import FormSendTweet from "../FormSendTweet";
+import { EventRounded } from "@mui/icons-material";
 
 export default function SendTweet() {
-
-  const[ isOpenModal, setIsOpenModal] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
   const openModal = () => {
-    setIsOpenModal(true)
-  }
+    setIsOpenModal(true);
+  };
   const closeModal = () => {
-    setIsOpenModal(false)
-  }
+    setIsOpenModal(false);
+  };
 
+  const sendTweet = () => {
+    event.preventDefault();
+    console.log("se envia");
+  };
   return (
-    <div className='send-tweet'>
-      <Fab className='send-tweet__open-modal' color="primary" aria-label='add' onClick={openModal}>
+    <div className="send-tweet">
+      <Fab
+        className="send-tweet__open-modal"
+        color="primary"
+        aria-label="add"
+        onClick={openModal}
+      >
         <AddIcon />
       </Fab>
       <ModalContainer isOpenModal={isOpenModal} closeModal={closeModal}>
-        <FormSendTweet />
+        <FormSendTweet sendTweet={sendTweet} />
       </ModalContainer>
     </div>
-  )
+  );
 }
